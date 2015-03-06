36c7695bd2fd538980f4903da0e7fc392a035a9e
gavellaneda <avellaneda.gabriel@gmail.com>
2015-03-06 21:42:29 +0000

    updated index.lua to user the HttpClient version from the framework

--- index.lua
+++ index.lua
@@ -8,66 +8,6 @@ local params = framework.boundary.param
 params.name = 'Spark Plugin'
 params.version = '1.0'
 
-function HttpPlugin:initialize(params)
-	Plugin.initialize(self, params)
-	
-	self.reqOptions = {
-		host = params.host,
-		port = params.port,
-		path = params.path
-	}
-end
-
-function HttpPlugin:error(err)
-	local msg = tostring(err)
-
-	print(msg)
-end
-
-function HttpPlugin:makeRequest(reqOptions, successCallback)
-	local req = http.request(reqOptions, function (res)
-
-
-		local data = ''
-		
-		res:on('data', function (chunk)
-			data = data .. chunk	
-			successCallback(data)
-			-- TODO: Verify when data its complete or when we need to use de end
-		end)
-
-		res:on('error', function (err)
-			local msg = 'Error while receiving a responde: ' .. err
-			self.error(msg)
-		end)
-
-	end)
-	
-	req:on('error', function (err)
-		local msg = 'Error while sending a request: ' .. err
-		self.error(msg)
-	end)
-
-	req:done()
-end
-
-function HttpPlugin:onPoll()
-	self:makeRequest(self.reqOptions, function (data)
-		self:parseResponse(data)
-	end)
-end
-
-function HttpPlugin:parseResponse(data)
-	local metrics = self:onParseResponse(data) 
-	self:report(metrics)
-end
-
-function HttpPlugin:onParseResponse(data)
-	-- To be overriden on class instance
-	print(data)
-	return {}
-end
-
 local plugin = HttpPlugin:new(params)
 
 function get(key, map)
